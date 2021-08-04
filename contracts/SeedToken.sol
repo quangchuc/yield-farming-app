97f2a43eb444d23aca1aec2f59270f1051879575
NGUYEN Quang Chuc <quangchuc91@gmail.com>
2021-08-05 01:49:54 +0200

    second commit

--- contracts/SeedToken.sol
+++ contracts/SeedToken.sol
@@ -6,8 +6,18 @@ import "./extensions/ERC20.sol";
 
 contract SeedToken is ERC20 {
     address public admin;
-    constructor() ERC20("Seed Token", "SEED");
-    _mint(msg.sender, 1000000 * 10 ** 18);
+    constructor() ERC20("Seed Token", "SEED") {
+        _mint(msg.sender, 1000000 * 10 ** 18);
+        admin = msg.sender;
+    }
+
+    function mint(address to, uint amount) external {
+        require(msg.sender == admin, "Only admin can do it");
+        _mint(to, amount);
+    }
     
+    function burn(address to, uint amount) external {
+        _burn(msg.sender, amount);
+    }
 
 }
