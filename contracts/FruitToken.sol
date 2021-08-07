162d1908d35b5661b6a4c8caf8335f4928ec780e
NGUYEN Quang Chuc <quangchuc91@gmail.com>
2021-08-08 00:19:32 +0200

    updates

--- contracts/FruitToken.sol
+++ contracts/FruitToken.sol
@@ -5,5 +5,16 @@ pragma solidity ^0.8.0;
 import "./extensions/ERC20.sol";
 
 contract FruitToken is ERC20 {
-    
+    address public admin; 
+
+    constructor() ERC20("Fruit Token", "FRUIT") {
+        _mint(msg.sender, 1000000 * 10 ** 18);
+        admin = msg.sender;
+    }
+
+    function mint(address to, uint amount) external {
+        require(msg.sender == admin, "Only admin can do it");
+        _mint(to, amount);
+    }
+
 }
---
+++ contracts/TokenFarm.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract TokenFarm {
+    string public name = "Yield farming app";
+
+}
