f8fa14db086d35a04e80eb2663f475d72712f9ee
NGUYEN Quang Chuc <quangchuc91@gmail.com>
2021-08-13 15:34:50 +0200

    changes

--- contracts/TokenFarm.sol
+++ contracts/TokenFarm.sol
@@ -14,14 +14,14 @@ contract TokenFarm {
     FruitToken public fruitToken;
     SeedToken public seedToken;
 
-    address[] public stakers;
-    mapping(address => uint) public stakingBalance;
-    mapping(address => bool) public hasStaked;
-    mapping(address => bool) public isStaking;
+    // address[] public stakers;
+    // mapping(address => uint) public stakingBalance;
+    // mapping(address => bool) public hasStaked;
+    // mapping(address => bool) public isStaking;
 
     // in constructor pass in the address for USDC token and your custom bank token
     // that will be used to pay interest
-    constructor(FruitToken _fruitToken, SeedToken _seedToken) public {
+    constructor(FruitToken _fruitToken, SeedToken _seedToken) {
         fruitToken = _fruitToken;
         seedToken = _seedToken;
         owner = msg.sender;
@@ -29,61 +29,61 @@ contract TokenFarm {
 
     // allow user to stake usdc tokens in contract
     
-    function stakeTokens(uint _amount) public {
+    // function stakeTokens(uint _amount) public {
 
-        require(_amount > 0, "amount cannot be 0");
+    //     require(_amount > 0, "amount cannot be 0");
 
-        // Trasnfer Seed tokens to contract for staking
-        seedToken.transferFrom(msg.sender, address(this), _amount);
+    //     // Trasnfer Seed tokens to contract for staking
+    //     seedToken.transferFrom(msg.sender, address(this), _amount);
 
-        // Update the staking balance in map
-        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+    //     // Update the staking balance in map
+    //     stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
 
-        // Add user to stakers array if they haven't staked already
-        if(!hasStaked[msg.sender]) {
-            stakers.push(msg.sender);
-        }
+    //     // Add user to stakers array if they haven't staked already
+    //     if(!hasStaked[msg.sender]) {
+    //         stakers.push(msg.sender);
+    //     }
 
-        // Update staking status to track
-        isStaking[msg.sender] = true;
-        hasStaked[msg.sender] = true;
-    }
+    //     // Update staking status to track
+    //     isStaking[msg.sender] = true;
+    //     hasStaked[msg.sender] = true;
+    // }
 
         // allow user to unstake total balance and withdraw USDC from the contract
     
-     function unstakeTokens() public {
+//      function unstakeTokens() public {
 
-    	// get the users staking balance in usdc
-    	uint balance = stakingBalance[msg.sender];
+//     	// get the users staking balance in usdc
+//     	uint balance = stakingBalance[msg.sender];
     
-        // reqire the amount staked needs to be greater then 0
-        require(balance > 0, "staking balance can not be 0");
+//         // reqire the amount staked needs to be greater then 0
+//         require(balance > 0, "staking balance can not be 0");
     
-        // transfer usdc tokens out of this contract to the msg.sender
-        seedToken.transfer(msg.sender, balance);
+//         // transfer usdc tokens out of this contract to the msg.sender
+//         seedToken.transfer(msg.sender, balance);
     
-        // reset staking balance map to 0
-        stakingBalance[msg.sender] = 0;
+//         // reset staking balance map to 0
+//         stakingBalance[msg.sender] = 0;
     
-        // update the staking status
-        isStaking[msg.sender] = false;
+//         // update the staking status
+//         isStaking[msg.sender] = false;
 
-} 
+// } 
 
 
     // Issue bank tokens as a reward for staking
     
-    function issueInterestToken() public {
-        for (uint i=0; i<stakers.length; i++) {
-            address recipient = stakers[i];
-            uint balance = stakingBalance[recipient];
+    // function issueInterestToken() public {
+    //     for (uint i=0; i<stakers.length; i++) {
+    //         address recipient = stakers[i];
+    //         uint balance = stakingBalance[recipient];
             
-    // if there is a balance transfer the SAME amount of bank tokens to the account that is staking as a reward
+    // // if there is a balance transfer the SAME amount of bank tokens to the account that is staking as a reward
             
-            if(balance >0 ) {
-                fruitToken.transfer(recipient, balance);
-            }
-        }
-    }
+    //         if(balance >0 ) {
+    //             fruitToken.transfer(recipient, balance);
+    //         }
+    //     }
+    // }
 
 }
